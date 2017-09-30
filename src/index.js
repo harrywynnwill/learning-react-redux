ddef3250a86d3a990270d8618844229a3a00448e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2017-09-30 12:23:13 +0100

    added an action factory tell the function what we want to do and with which data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import {CREATE_POST} from './action-types'
+function createPost (user, text){
+  return {
+    type: CREATE_POST,
+    user: user,
+    text: text
+  }
+}
+console.log(createPost("Dan", "Hello World!"))
